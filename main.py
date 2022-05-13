596741aeb0fb1cf8230795e1066d649607f9d77f
Nienke <93122759+nienkel@users.noreply.github.com>
2022-05-13 08:02:38 +0000

    Nog meer questions, ook elif toegevoegd....... yeah

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-print("Welcome!")
+print("Welcome by Lucifer!")
 print()
 naam = input("Please enter your name:")
 print(naam)
 
 question = input("Would you like to play this game? type yes or no\n")
-if question == "Yes":
+if question == "yes":
   print("Nice! Lets get started")
-
+elif question == "no":
+  print("O... Ok..")
+question2 = input("Are you sure about that?")
