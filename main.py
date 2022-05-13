c6cee3c9d76e27686700b371e47ed85ca60c0fa3
Nienke <93122759+nienkel@users.noreply.github.com>
2022-05-13 07:51:21 +0000

    start game

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("yay")
+print("Welcome!")
+print()
+naam = input("Please enter your name:")
+print(naam)
+
+question = input("Would you like to play this game? type yes or no\n")
+if question == "Yes":
+  print("Nice! Lets get started")
+
