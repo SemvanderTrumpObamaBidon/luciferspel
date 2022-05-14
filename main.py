3dfb242c033b6891585fc0c29621bd94b70ddc68
Nienke <93122759+nienkel@users.noreply.github.com>
2022-05-14 12:41:58 +0000

    .

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 print("Welcome by Lucifer!")
 print()
-naam = input("Please enter your name:")
-print(naam)
+naam = input("Please enter your name:\n")
 
-question = input("Would you like to play this game? type yes or no\n")
+question = input ("Would you like to play this game? type yes or no\n")
 if question == "yes":
   print("Nice! Lets get started")
 elif question == "no":
-  print("O... Ok..")
-question2 = input("Are you sure about that?")
+  print("O... Come on... Pls?")
+  question = input ("Would you like to play this game? type yes or no\n")
+if question == "yes":
+  print("Finally! Lets go!") 
+elif question == "no":
+  print("fuck you....") 
+
+print ("The game is easy, ")
