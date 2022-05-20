0afee6ce2e46533c465793370a5e5b9de7314ebc
sem van klink <93123063+SemvanderTrumpObamaBidon@users.noreply.github.com>
2022-05-20 07:33:44 +0000

    .

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("Welcome by Lucifer!")
 print()
 naam = input("Please enter your name:\n")
 
-question = input ("Would you like to play this game? type yes or no\n")
+  question = input ("Would you like to play this game? type yes or no\n")
 if question == "yes":
   print("Nice! Lets get started")
 elif question == "no":
