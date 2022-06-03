55ed1394014268a7bc96a928eb0eabf39ca3d959
sem van klink <93123063+SemvanderTrumpObamaBidon@users.noreply.github.com>
2022-06-03 06:48:02 +0000

    weer opnieuw begonnen

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-print("Welcome by Lucifer!")
-print("-Welcom-De computer kiest een willekeurig aantal lucifers tussen de 20 en 25. Dat aantal wordt door de computer als getal aan je getoond. Als speler heb je als eerste de keuze om 1, 2 of 3 lucifers weg te nemen. Daarna is het de beurt aan de computer. Ook de computer moet 1, 2 of 3 lucifers wegnemen, waarna de speler weer aan de beurt is, etc. Degene die als laatste een lucifer MOET wegnemen, heeft verloren. De winnaar van het spel wordt getoond.")
-x = randint(20, 25)    
-print(x)
+pprint("Welcome by Lucifer!")
+print()
+naam = input("Please enter your name:\n")
+
+question = input ("Would you like to play this game? type yes or no\n")
+if question == "yes":
+  print("Nice! Lets get started")
+elif question == "no":
+  print("O... Come on... Pls?")
+  question = input ("Would you like to play this game? type yes or no\n")
+
+if question == "yes":
+  print("Finally! Lets go!") 
+
+elif question == "no":
+  print("fuck you....you're gonna play it anyway") 
+
+
+print ("The game is easy, ")
+
 
 
