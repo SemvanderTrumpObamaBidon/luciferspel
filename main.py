b5d869a42906d37b01d86572614a46f764b09331
Nienke <93122759+nienkel@users.noreply.github.com>
2022-06-03 07:14:54 +0000

    HIJ WERKT MET DE FUNCTION

--- main.py
+++ main.py
@@ -1,30 +1,23 @@
-pprint("Welcome by Lucifer!")
-print()
-naam = input("Please enter your name:\n")
+print("Welcome by Lucifer!")
+def game():
+  print()
+  naam = input("Please enter your name:\n")
 
-question = input ("Would you like to play this game? type yes or no\n")
-if question == "yes":
-  print("Nice! Lets get started")
-elif question == "no":
-  print("O... Come on... Pls?")
   question = input ("Would you like to play this game? type yes or no\n")
-
-if question == "yes":
-  print("Finally! Lets go!") 
-
-elif question == "no":
-  print("fuck you....you're gonna play it anyway") 
-
-
-print ("The game is easy, ")
+  if question == "yes":
+    print("Nice! Lets get started")
+  elif question == "no":
+    print("Are you sure? lets try this again!") 
+    game()
+game()
 
 
 
 
 
 
-Beschrijving van het spel
-De computer kiest een willekeurig aantal lucifers tussen de 20 en 25. Dat aantal wordt door de computer als getal aan je getoond. Als speler heb je als eerste de keuze om 1, 2 of 3 lucifers weg te nemen. Daarna is het de beurt aan de computer. Ook de computer moet 1, 2 of 3 lucifers wegnemen, waarna de speler weer aan de beurt is, etc. Degene die als laatste een lucifer MOET wegnemen, heeft verloren. De winnaar van het spel wordt getoond.
+
+
 
 
 
