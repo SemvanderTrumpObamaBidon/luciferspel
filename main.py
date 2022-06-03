38e6fdf9ccb5bd5e79efb852d1f7577bc30f9677
Nienke <93122759+nienkel@users.noreply.github.com>
2022-06-03 08:04:22 +0000

    Proberen ervoor te zorgen dat er een min en max is van afnemen lucifers

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ print(f"de computer heeft {aantalLucifers} lucifers gekozen")
 def game():
   global aantalLucifers
   print()
-  x = input("Hoeveel lucifers wil je weghalen? \n")
+  x = input ("Hoeveel lucifers wil je weghalen? kies tussen 1,2 of 3 \n")
+  weghalen= int(input())
+  if( take < 1 or take > 3 )
   
   aantalLucifers = aantalLucifers - int(x)
   print(f"Nog {aantalLucifers} lucifers over")  
