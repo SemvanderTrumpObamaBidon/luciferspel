6de7a607779ae8cd3df0678b778847fd421caf57
Nienke <93122759+nienkel@users.noreply.github.com>
2022-06-05 13:06:15 +0000

    de speler kan nu als correct doen/uitvoeren

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import random
 
-naam = ""
-aantalLucifers = random.randint(20,25)
-print(f"de computer heeft {aantalLucifers} lucifers gekozen")
+def intro():
+  naam = input ("Hoi, wat is je naam?\n")
+  aantalLucifers = random.randint(20,25)
+  print(f"de computer heeft {aantalLucifers} lucifers gekozen")
+  return naam, aantalLucifers
+  
+def speler():
+  x = ""
+  while True:
+    x = input (f"Hoeveel lucifers wil je weghalen {naam}? kies tussen 1,2 of 3 \n")
+
+    if x in ["1", "2", "3"]:
+      x = int(x)
+      if x <= aantalLucifers:
+        return x
+      else:
+        print("Je kunt niet meer lucifers weghalen dan die er zijn, sukkel....")
 
 def game():
   global aantalLucifers
   print()
 
-  x = ""
-  while x not in["1", "2", "3"]:
-    x = input ("Hoeveel lucifers wil je weghalen? kies tussen 1,2 of 3 \n")
+  x = speler()
 
-  aantalLucifers = aantalLucifers - int(x)
+  aantalLucifers = aantalLucifers - x
   print(f"Nog {aantalLucifers} lucifers over")  
 
   if aantalLucifers > 0:
@@ -20,6 +32,7 @@ def game():
   else:
     print("GAME OVER!")
 
+naam, aantalLucifers = intro()
 game()
 
 
